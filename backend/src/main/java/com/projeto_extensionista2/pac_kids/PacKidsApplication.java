70351e410e5cfbc463d7815cab3d3e6e330c5073
Leo Marinho <dev.leomarinho@gmail.com>
2024-10-27 22:20:43 -0300

    adiciona a pasta backend e frontend na branch dev

---
+++ backend/src/main/java/com/projeto_extensionista2/pac_kids/PacKidsApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_extensionista2.pac_kids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacKidsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto_extensionista2/pac_kids/PacKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_extensionista2.pac_kids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
